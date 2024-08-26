3a2efb9463820df4ba80424c2d77d6bc166450e8
Mohamed <mohamedjoumoi@gmail.com>
2024-08-26 02:18:03 +0200

    feat: pwa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Importation du service worker
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
+
+// Enregistrer le service worker pour activer le mode hors-ligne et d'autres fonctionnalités PWA
+serviceWorkerRegistration.register();
